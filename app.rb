2e9e736570f68fac211b2f9105f0006a93421725
Thomas Gray <tgray017@gmail.com>
2019-04-10 11:30:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ class App < Sinatra::Base
   
   post '/puppy' do
     puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = puppy.name
+    @breed = puppy.breed
+    @age = puppy.months_old
     erb :display_puppy
   end
 
