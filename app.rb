a7f8bf4c93421c890554ebf8ac3a9f99267210cd
Thomas Gray <tgray017@gmail.com>
2019-04-10 11:33:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/puppy' do
+    puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = puppy.name
+    @breed = puppy.breed
+    @age = puppy.months_old
+    erb :display_puppy
+  end
 
 end
